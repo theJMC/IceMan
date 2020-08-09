a149f93fa79ec9209d6b0b1a06b158b23d825ccc
James (WSL) <jamesmccarthy150@gmail.com>
2020-08-09 14:19:18 +0100

    added bold for Usernames

--- main.js
+++ main.js
@@ -4,11 +4,16 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const hbs = require("express-handlebars");
+const cParser = require("cookie-parser");
 
 // Config
 
 
-const port = 80;
+const port = 1337;
+
+// Add CookieParser to the Express App
+app.use(cParser());
+
 
 // Setup Handlebars View Engine
 app.set("view engine", "hbs");
@@ -25,7 +30,33 @@ app.use(express.static(__dirname + "/public"));
 // |-- DEFINING VIEWS --| 
 
 app.get("/", (req, res) => {
-    res.render('index', {title: "Chat", active: {"chat": true}})
+    try{
+        if (req.header("Referer").split("/")[3] == "chat"){
+            res.render("index", {title: "Home", active: {"home" : true}})
+        } else {
+            if (req.cookies.usr == null){
+                res.render("index", {title: "Home", active: {"home" : true}})
+            } else {
+                res.redirect("/chat")
+            }
+        }
+    } catch(err) {
+        if (req.cookies.usr == null){
+            res.render("index", {title: "Home", active: {"home" : true}})
+        } else {
+            res.redirect("/chat")
+        }
+    }
+    
+})
+
+app.get("/chat", (req, res) => {
+    if (req.cookies.usr != null){
+        res.render('chat', {title: "Chat", active: {"chat": true, "resetRedirect": true}})
+    } else {
+        res.redirect("/chat")
+    }
+    
 })
 
 io.on("connection", socket => {
@@ -33,15 +64,15 @@ io.on("connection", socket => {
     socket.on("nickname", (nickname) => {
         nick = nickname;
         console.log(nick + " Connected");
-        io.emit("chat message", nick + " joined the session");
+        io.emit("chat message", {usr: "SERVER", msg: nick + " joined the session"} );
     })
     socket.on('disconnect', () => {
-        console.log("User Disconnected");
-        io.emit("chat message", nick + " Disconnected")
+        console.log(nick + " Disconnected");
+        io.emit("chat message", {usr: "SERVER", msg: nick + " Disconnected"})
     })
     socket.on("chat message", msg => {
         console.log(nick + ": " + msg.msg);
-        io.emit("chat message", nick + ": " + msg.msg);
+        io.emit("chat message", {usr: nick, msg: msg.msg});
     })
 })
 
